72ee5d610f638027ee1da66d7f6b988e23de2931
qq-me <nkshv2@gmail.com>
2024-12-01 19:22:57 +0300

    update cautious

--- src/torchzero/grad/derivatives.py
+++ src/torchzero/grad/derivatives.py
@@ -33,19 +33,68 @@ def _jacobian_batched(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch
         is_grads_batched=True,
     )
 
-def jacobian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], create_graph=False, batched=True):
+def jacobian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], create_graph=False, batched=True) -> abc.Sequence[torch.Tensor]:
+    """Calculate jacobian of a sequence of tensors w.r.t another sequence of tensors.
+    Returns a sequence of tensors with the length as `wrt`.
+    Each tensor will have the shape `(*input.shape, *wrt[i].shape)`.
+
+    Args:
+        input (abc.Sequence[torch.Tensor]): input sequence of tensors.
+        wrt (abc.Sequence[torch.Tensor]): sequence of tensors to differentiate w.r.t.
+        create_graph (bool, optional):
+            pytorch option, if True, graph of the derivative will be constructed,
+            allowing to compute higher order derivative products. Default: False.
+        batched (bool, optional): use faster but experimental pytorch batched jacobian
+            This only has effect when `input` has more than 1 element. Defaults to True.
+
+    Returns:
+        sequence of tensors with the length as `wrt`.
+    """
     if batched: return _jacobian_batched(input, wrt, create_graph)
     return _jacobian(input, wrt, create_graph)
 
-def hessian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], batched=True):
-    return jacobian(jacobian(input, wrt, create_graph=True, batched=batched), wrt, batched=batched)
+def hessian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], create_graph=False, batched=True):
+    """Calculate hessian of a sequence of tensors w.r.t another sequence of tensors.
+    Returns a sequence of tensors with the length as `wrt`.
+    If you need a hessian matrix out of that sequence, pass it to `hessian_list_to_mat`.
 
-def jacobian_and_hessian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], batched=True):
+    Args:
+        input (abc.Sequence[torch.Tensor]): input sequence of tensors.
+        wrt (abc.Sequence[torch.Tensor]): sequence of tensors to differentiate w.r.t.
+        create_graph (bool, optional):
+            pytorch option, if True, graph of the derivative will be constructed,
+            allowing to compute higher order derivative products. Default: False.
+        batched (bool, optional): use faster but experimental pytorch batched grad. Defaults to True.
+
+    Returns:
+        sequence of tensors with the length as `wrt`.
+    """
+    return jacobian(jacobian(input, wrt, create_graph=True, batched=batched), wrt, create_graph=create_graph, batched=batched)
+
+def jacobian_and_hessian(input: abc.Sequence[torch.Tensor], wrt: abc.Sequence[torch.Tensor], create_graph=False, batched=True):
+    """Calculate jacobian and hessian of a sequence of tensors w.r.t another sequence of tensors.
+    Calculating hessian requires calculating the jacobian. So this function is more efficient than
+    calling `jacobian` and `hessian` separately, which would calculate jacobian twice.
+
+    Args:
+        input (abc.Sequence[torch.Tensor]): input sequence of tensors.
+        wrt (abc.Sequence[torch.Tensor]): sequence of tensors to differentiate w.r.t.
+        create_graph (bool, optional):
+            pytorch option, if True, graph of the derivative will be constructed,
+            allowing to compute higher order derivative products. Default: False.
+        batched (bool, optional): use faster but experimental pytorch batched grad. Defaults to True.
+
+    Returns:
+        tuple with jacobians sequence and hessians sequence.
+    """
     jac = jacobian(input, wrt, create_graph=True, batched = batched)
-    return jac, jacobian(jac, wrt, batched = batched)
+    return jac, jacobian(jac, wrt, batched = batched, create_graph=create_graph)
 
-def jacobian_list_to_vec(jacobians: abc.Sequence[torch.Tensor]):
+def jacobian_list_to_vec(jacobians: abc.Iterable[torch.Tensor]):
+    """flattens and concatenates a sequence of tensors."""
     return torch.cat([i.ravel() for i in jacobians], 0)
 
 def hessian_list_to_mat(hessians: abc.Sequence[torch.Tensor]):
+    """takes output of `hessian` and returns the 2D hessian matrix.
+    Note - I only tested this for cases where input is a scalar."""
     return torch.cat([h.reshape(h.size(0), h[1].numel()) for h in hessians], 1)
--- src/torchzero/modules/adaptive/cautious.py
+++ src/torchzero/modules/adaptive/cautious.py
@@ -6,9 +6,9 @@ from ...tensorlist import TensorList
 from ...core import OptimizerModule
 
 class Cautious(OptimizerModule):
-    def __init__(self):
+    def __init__(self, eps=1e-6):
         """
-        Zeroes updates for parameters where ascent direction (momentum) sign is inconsistent with the gradient sign.
+        Applies mask function based on the sign consistency of ascent direction and gradient.
 
         This method has been described in *Cautious Optimizers: Improving Training with One Line of Code.
         Kaizhao Liang, Lizhang Chen, Bo Liu, Qiang Liu*
@@ -18,13 +18,13 @@ class Cautious(OptimizerModule):
 
         """
         super().__init__({})
+        self.eps = eps
 
     @torch.no_grad
     def _update(self, state, ascent_direction):
         params = self.get_params()
         grad = state.maybe_compute_grad_(params)
-
-        mask = grad.sign().eq(ascent_direction.sign())
-        ascent_direction *= mask
+        mask = (ascent_direction * grad > 0).to(grad.dtype)
+        ascent_direction *= mask / (mask.total_mean() + self.eps)
 
         return ascent_direction
