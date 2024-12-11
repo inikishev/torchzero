e666a060f4ae0d79247a0034aaccf2a73444ee39
qq-me <nkshv2@gmail.com>
2024-12-11 18:09:04 +0300

    centered rmsprop

--- src/torchzero/modules/optimizers/rmsprop.py
+++ src/torchzero/modules/optimizers/rmsprop.py
@@ -5,13 +5,26 @@ import torch
 from ...tensorlist import TensorList
 from ...core import OptimizerModule
 
+class _tensor_like:
+    """_foreach_lerp_ doesn't accept list of scalars, so I have to convert them to tensors
+    meaning I have to give them appropriate dtype and device."""
+    __slots__ = ("device", "dtype")
+    def __init__(self, tensor):
+        self.device = tensor.device
+        self.dtype = tensor.dtype
+    def __call__(self, x): return torch.tensor(x, dtype = self.dtype, device = self.device)
 
-def _rmsprop_step(ascent: TensorList, mean_sqr: TensorList, alpha, eps: TensorList):
+def _rmsprop_step_(ascent: TensorList, mean_sqr: TensorList, alpha, eps: TensorList):
     mean_sqr.mul_(alpha).addcmul_(ascent, ascent, value = 1 - alpha)
     return ascent.div_(mean_sqr.sqrt().add_(eps))
 
+def _centered_rmsprop_step_(ascent: TensorList, mean_sqr: TensorList, mean: TensorList, alpha, eps: TensorList):
+    mean_sqr.mul_(alpha).addcmul_(ascent, ascent, value = 1 - alpha)
+    mean.lerp_(ascent, 1-alpha.map(_tensor_like(ascent[0])))
+    return ascent.div_(mean_sqr.addcmul(mean, mean, value=-1).sqrt_().add_(eps))
+
 class RMSProp(OptimizerModule):
-    def __init__(self, alpha: float = 0.99, eps: float = 1e-8):
+    def __init__(self, alpha: float = 0.99, eps: float = 1e-8, centered=False):
         """https://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf
 
         Exactly matches pytorch RMSProp.
@@ -22,10 +35,15 @@ class RMSProp(OptimizerModule):
         """
         defaults = dict(alpha = alpha, eps = eps)
         super().__init__(defaults)
+        self.centered = centered
 
     @torch.no_grad
     def _update(self, state, ascent):
-        mean_sqr = self.get_state_key('mean_sqr')
         settings = self.get_all_group_keys()
-        updated_direction = _rmsprop_step(ascent, mean_sqr, settings['alpha'], settings['eps'])
+        if self.centered:
+            mean, mean_sqr = self.get_state_keys(('mean', 'mean_sqr'))
+            updated_direction = _centered_rmsprop_step_(ascent, mean_sqr, mean, settings['alpha'], settings['eps'])
+        else:
+            mean_sqr = self.get_state_key('mean_sqr')
+            updated_direction = _rmsprop_step_(ascent, mean_sqr, settings['alpha'], settings['eps'])
         return updated_direction
