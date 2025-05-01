b6a5d959edfa229ea2bbc2f2899a521bcac37b06
inikishev <nkshv2@gmail.com>
2025-05-01 13:48:15 +0300

    experimental __init__

---
+++ torchzero/modules/experimental/__init__.py
@@ -0,0 +1,4 @@
+from .gradmin import GradMin
+from .minibatch_rprop import MinibatchRprop
+from .reduce_outward_lr import ReduceOutwardLR
+from .subspace_preconditioners import RandomPreconditioning, HistoryPreconditioning
