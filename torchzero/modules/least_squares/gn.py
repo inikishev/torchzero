4fc4fb08a8679697f6239407b01b501e96073e5c
inikishev <nkshv2@gmail.com>
2025-07-20 17:55:42 +0300

    GaussNewton docstring

--- torchzero/modules/least_squares/gn.py
+++ torchzero/modules/least_squares/gn.py
@@ -5,8 +5,13 @@ from ...utils.derivatives import jacobian_wrt, flatten_jacobian
 from ...utils import vec_to_tensors
 from ...utils.linalg import linear_operator
 class SumOfSquares(Module):
-    """Sets loss to be the sum of squares of losses, this is meant to be used to test least squares methods against
-    ordinary minimization methods."""
+    """Sets loss to be the sum of squares of values returned by the closure.
+
+    This is meant to be used to test least squares methods against ordinary minimization methods.
+
+    To use this, the closure should return a vector of values to minimize sum of squares of.
+    Please add the `backward` argument, it will always be False but it is required.
+    """
     def __init__(self):
         super().__init__()
 
@@ -39,7 +44,58 @@ class SumOfSquares(Module):
 
 
 class GaussNewton(Module):
+    """Gauss-newton method.
+
+    To use this, the closure should return a vector of values to minimize sum of squares of.
+    Please add the `backward` argument, it will always be False but it is required.
+    Gradients will be calculated via batched autograd within this module, you don't need to
+    implement the backward pass. Please see below for an example.
+
+    .. note::
+
+        This method requires ``ndim^2`` memory, however, if it is used within ``tz.m.TrustCG`` trust region,
+        the memory requirement is ``ndim*m``, where ``m`` is number of values in the output.
+
+    Args:
+        reg (float, optional): regularization parameter. Defaults to 1e-8.
+        batched (bool, optional): whether to use vmapping. Defaults to True.
+
+    Examples:
+
+        minimizing the rosenbrock function:
+
+        .. code:: py
+
+            def rosenbrock(X):
+                x1, x2 = X
+                return torch.stack([(1 - x1)**2, 100 * (x2 - x1**2)**2])
+
+            X = torch.tensor([-1.1, 2.5], requires_grad=True)
+            opt = tz.Modular([X], tz.m.GaussNewton(), tz.m.Backtracking())
+
+            # define the closure
+            def closure(backward=True):
+                return rosenbrock(X)
+
+            # minimize
+            for iter in range(10):
+                loss = opt.step(closure)
+                print(f'{loss = }')
+
+        Memory-efficient (if n << ndim) GN with trust region:
+
+        .. code:: py
+
+            opt = tz.Modular(
+                model.parameters(),
+                tz.m.TrustCG(tz.m.GaussNewton())
+            )
+
+    """
     def __init__(self, reg:float = 1e-8, batched:bool=True, ):
+        """_summary_
+
+        """
         super().__init__(defaults=dict(batched=batched, reg=reg))
 
     @torch.no_grad
