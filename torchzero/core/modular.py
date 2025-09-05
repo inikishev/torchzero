522a6a69386de5460f3d3e14f5302f84dbfe5eb4
Ivan Nikishev <nkshv2@gmail.com>
2025-09-05 10:14:24 +0300

    move step logic into separate function

--- torchzero/core/modular.py
+++ torchzero/core/modular.py
@@ -59,6 +59,28 @@ def unroll_modules(*modules: Chainable) -> list[Module]:
     return unrolled
 
 
+def step(var: Var, modules: Sequence[Module], is_nested:bool = False):
+    n_modules = len(modules)
+    if n_modules == 0: return var.clone(clone_update=False)
+
+    last_module = modules[-1]
+    last_lr = last_module.defaults.get('lr', None)
+
+    # step
+    for i, module in enumerate(modules):
+        if i!=0: var = var.clone(clone_update=False)
+
+        # last module, or next to last module before lr
+        if (i == n_modules - 1) or ((i == n_modules - 2) and (last_lr is not None)):
+            if len(module.children) != 0 or is_nested: var.nested_is_last = True
+            else: var.is_last = True
+            if last_lr is not None: var.last_module_lrs = [last_module.settings[p]['lr'] for p in var.params]
+
+        var = module.step(var)
+        if var.stop: break
+
+    return var
+
 # have to inherit from Modular to support lr schedulers
 # although Accelerate doesn't work due to converting param_groups to a dict
 class Modular(torch.optim.Optimizer):
@@ -207,23 +229,10 @@ class Modular(torch.optim.Optimizer):
             var.grad = [p.grad if p.grad is not None else torch.zeros_like(p) for p in params]
             self.num_evaluations += 1
 
-        n_modules = len(self.modules)
-        if n_modules == 0: raise RuntimeError("There are no modules in this `Modular` optimizer")
-        last_module = self.modules[-1]
-        last_lr = last_module.defaults.get('lr', None)
+        if len(self.modules) == 0: raise RuntimeError("There are no modules in this `Modular` optimizer")
 
         # step
-        for i, module in enumerate(self.modules):
-            if i!=0: var = var.clone(clone_update=False)
-
-            # last module, or next to last module before lr
-            if (i == n_modules - 1) or ((i == n_modules - 2) and (last_lr is not None)):
-                if module.children: var.nested_is_last = True
-                else: var.is_last = True
-                if last_lr is not None: var.last_module_lrs = [last_module.settings[p]['lr'] for p in var.params]
-
-            var = module.step(var)
-            if var.stop: break
+        var = step(var, self.modules, is_nested=False)
 
         # apply update
         if not var.skip_update:
