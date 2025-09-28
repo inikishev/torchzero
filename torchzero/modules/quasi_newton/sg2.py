d3199c5fb6a0c214c9eb9246f6df7f7f8e09f707
Ivan Nikishev <nkshv2@gmail.com>
2025-09-28 19:13:56 +0300

    updated SG2 to be a Transform; removed SPSA2 as it is made redundant by SG2

--- torchzero/modules/quasi_newton/sg2.py
+++ torchzero/modules/quasi_newton/sg2.py
@@ -1,8 +1,9 @@
 import torch
 
-from ...core import Module, Chainable, step
-from ...utils import TensorList, vec_to_tensors
-from ..second_order.newton import _newton_step, _get_H
+from ...core import Chainable, Transform
+from ...utils import TensorList, unpack_dicts, unpack_states, vec_to_tensors
+from ..second_order.newton import _get_H, _newton_step
+
 
 def sg2_(
     delta_g: torch.Tensor,
@@ -18,8 +19,18 @@ def sg2_(
 
 
 
-class SG2(Module):
+class SG2(Transform):
     """second-order stochastic gradient
+
+    2SPSA (second-order SPSA)
+    ```python
+    opt = tz.Modular(
+        model.parameters(),
+        tz.m.SPSA(),
+        tz.m.SG2(),
+        tz.m.LR(1e-2),
+    )
+    ```
 
     SG2 with line search
     ```python
@@ -50,17 +61,16 @@ class SG2(Module):
         one_sided: bool = False, # one-sided hessian
         use_lstsq: bool = True,
         seed=None,
+        update_freq: int = 1,
         inner: Chainable | None = None,
     ):
         defaults = dict(n_samples=n_samples, h=h, beta=beta, damping=damping, eigval_fn=eigval_fn, one_sided=one_sided, seed=seed, use_lstsq=use_lstsq)
-        super().__init__(defaults)
-
-        if inner is not None: self.set_child('inner', inner)
+        super().__init__(defaults, update_freq=update_freq, inner=inner)
 
     @torch.no_grad
-    def update(self, objective):
-        k = self.global_state.get('step', 0) + 1
-        self.global_state["step"] = k
+    def update_states(self, objective, states, settings):
+        fs = settings[0]
+        k = self.increment_counter("step", 0)
 
         params = TensorList(objective.params)
         closure = objective.closure
@@ -68,9 +78,9 @@ class SG2(Module):
             raise RuntimeError("closure is required for SG2")
         generator = self.get_generator(params[0].device, self.defaults["seed"])
 
-        h = self.get_settings(params, "h")
+        h = unpack_dicts(settings, "h")
         x_0 = params.clone()
-        n_samples = self.defaults["n_samples"]
+        n_samples = fs["n_samples"]
         H_hat = None
 
         for i in range(n_samples):
@@ -78,7 +88,7 @@ class SG2(Module):
             cd = params.rademacher_like(generator=generator).mul_(h)
 
             # one sided
-            if self.defaults["one_sided"]:
+            if fs["one_sided"]:
                 g_0 = TensorList(objective.get_grads())
                 params.add_(cd)
                 closure()
@@ -118,23 +128,23 @@ class SG2(Module):
         H = self.global_state.get("H", None)
         if H is None: H = H_hat
         else:
-            beta = self.defaults["beta"]
-            if beta is None: beta = k / (k+1)
+            beta = fs["beta"]
+            if beta is None: beta = (k+1) / (k+2)
             H.lerp_(H_hat, 1-beta)
 
         self.global_state["H"] = H
 
 
     @torch.no_grad
-    def apply(self, objective):
+    def apply_states(self, objective, states, settings):
+        fs = settings[0]
         dir = _newton_step(
             objective=objective,
             H = self.global_state["H"],
-            damping = self.defaults["damping"],
-            inner = self.children.get("inner", None),
+            damping = fs["damping"],
             H_tfm=None,
-            eigval_fn=self.defaults["eigval_fn"],
-            use_lstsq=self.defaults["use_lstsq"],
+            eigval_fn=fs["eigval_fn"],
+            use_lstsq=fs["use_lstsq"],
             g_proj=None,
         )
 
@@ -147,146 +157,146 @@ class SG2(Module):
 
 
 
-# two sided
-# we have g via x + d, x - d
-# H via g(x + d), g(x - d)
-# 1 is x, x+2d
-# 2 is x, x-2d
-# 5 evals in total
+# # two sided
+# # we have g via x + d, x - d
+# # H via g(x + d), g(x - d)
+# # 1 is x, x+2d
+# # 2 is x, x-2d
+# # 5 evals in total
 
-# one sided
-# g via x, x + d
-# 1 is x, x + d
-# 2 is x, x - d
-# 3 evals and can use two sided for g_0
+# # one sided
+# # g via x, x + d
+# # 1 is x, x + d
+# # 2 is x, x - d
+# # 3 evals and can use two sided for g_0
 
-class SPSA2(Module):
-    """second-order SPSA
+# class SPSA2(Module):
+#     """second-order SPSA
 
-    SPSA2 with line search
-    ```python
-    opt = tz.Modular(
-        model.parameters(),
-        tz.m.SPSA2(),
-        tz.m.Backtracking()
-    )
-    ```
+#     SPSA2 with line search
+#     ```python
+#     opt = tz.Modular(
+#         model.parameters(),
+#         tz.m.SPSA2(),
+#         tz.m.Backtracking()
+#     )
+#     ```
 
-    SPSA2 with trust region
-    ```python
-    opt = tz.Modular(
-        model.parameters(),
-        tz.m.LevenbergMarquardt(tz.m.SPSA2()),
-    )
-    ```
-    """
+#     SPSA2 with trust region
+#     ```python
+#     opt = tz.Modular(
+#         model.parameters(),
+#         tz.m.LevenbergMarquardt(tz.m.SPSA2()),
+#     )
+#     ```
+#     """
 
-    def __init__(
-        self,
-        n_samples: int = 1,
-        h: float = 1e-2,
-        beta: float | None = None,
-        damping: float = 0,
-        eigval_fn=None,
-        use_lstsq: bool = True,
-        seed=None,
-        inner: Chainable | None = None,
-    ):
-        defaults = dict(n_samples=n_samples, h=h, beta=beta, damping=damping, eigval_fn=eigval_fn, seed=seed, use_lstsq=use_lstsq)
-        super().__init__(defaults)
+#     def __init__(
+#         self,
+#         n_samples: int = 1,
+#         h: float = 1e-2,
+#         beta: float | None = None,
+#         damping: float = 0,
+#         eigval_fn=None,
+#         use_lstsq: bool = True,
+#         seed=None,
+#         inner: Chainable | None = None,
+#     ):
+#         defaults = dict(n_samples=n_samples, h=h, beta=beta, damping=damping, eigval_fn=eigval_fn, seed=seed, use_lstsq=use_lstsq)
+#         super().__init__(defaults)
 
-        if inner is not None: self.set_child('inner', inner)
+#         if inner is not None: self.set_child('inner', inner)
 
-    @torch.no_grad
-    def update(self, objective):
-        k = self.global_state.get('step', 0) + 1
-        self.global_state["step"] = k
+#     @torch.no_grad
+#     def update(self, objective):
+#         k = self.global_state.get('step', 0) + 1
+#         self.global_state["step"] = k
 
-        params = TensorList(objective.params)
-        closure = objective.closure
-        if closure is None:
-            raise RuntimeError("closure is required for SPSA2")
+#         params = TensorList(objective.params)
+#         closure = objective.closure
+#         if closure is None:
+#             raise RuntimeError("closure is required for SPSA2")
 
-        generator = self.get_generator(params[0].device, self.defaults["seed"])
+#         generator = self.get_generator(params[0].device, self.defaults["seed"])
 
-        h = self.get_settings(params, "h")
-        x_0 = params.clone()
-        n_samples = self.defaults["n_samples"]
-        H_hat = None
-        g_0 = None
+#         h = self.get_settings(params, "h")
+#         x_0 = params.clone()
+#         n_samples = self.defaults["n_samples"]
+#         H_hat = None
+#         g_0 = None
 
-        for i in range(n_samples):
-            # perturbations for g and H
-            cd_g = params.rademacher_like(generator=generator).mul_(h)
-            cd_H = params.rademacher_like(generator=generator).mul_(h)
+#         for i in range(n_samples):
+#             # perturbations for g and H
+#             cd_g = params.rademacher_like(generator=generator).mul_(h)
+#             cd_H = params.rademacher_like(generator=generator).mul_(h)
 
-            # evaluate 4 points
-            x_p = x_0 + cd_g
-            x_n = x_0 - cd_g
+#             # evaluate 4 points
+#             x_p = x_0 + cd_g
+#             x_n = x_0 - cd_g
 
-            params.set_(x_p)
-            f_p = closure(False)
-            params.add_(cd_H)
-            f_pp = closure(False)
+#             params.set_(x_p)
+#             f_p = closure(False)
+#             params.add_(cd_H)
+#             f_pp = closure(False)
 
-            params.set_(x_n)
-            f_n = closure(False)
-            params.add_(cd_H)
-            f_np = closure(False)
+#             params.set_(x_n)
+#             f_n = closure(False)
+#             params.add_(cd_H)
+#             f_np = closure(False)
 
-            g_p_vec = (f_pp - f_p) / cd_H
-            g_n_vec = (f_np - f_n) / cd_H
-            delta_g = g_p_vec - g_n_vec
+#             g_p_vec = (f_pp - f_p) / cd_H
+#             g_n_vec = (f_np - f_n) / cd_H
+#             delta_g = g_p_vec - g_n_vec
 
-            # restore params
-            params.set_(x_0)
+#             # restore params
+#             params.set_(x_0)
 
-            # compute grad
-            g_i = (f_p - f_n) / (2 * cd_g)
-            if g_0 is None: g_0 = g_i
-            else: g_0 += g_i
+#             # compute grad
+#             g_i = (f_p - f_n) / (2 * cd_g)
+#             if g_0 is None: g_0 = g_i
+#             else: g_0 += g_i
 
-            # compute H hat
-            H_i = sg2_(
-                delta_g = delta_g.to_vec().div_(2.0),
-                cd = cd_g.to_vec(), # The interval is measured by the original 'cd'
-            )
-            if H_hat is None: H_hat = H_i
-            else: H_hat += H_i
+#             # compute H hat
+#             H_i = sg2_(
+#                 delta_g = delta_g.to_vec().div_(2.0),
+#                 cd = cd_g.to_vec(), # The interval is measured by the original 'cd'
+#             )
+#             if H_hat is None: H_hat = H_i
+#             else: H_hat += H_i
 
-        assert g_0 is not None and H_hat is not None
-        if n_samples > 1:
-            g_0 /= n_samples
-            H_hat /= n_samples
+#         assert g_0 is not None and H_hat is not None
+#         if n_samples > 1:
+#             g_0 /= n_samples
+#             H_hat /= n_samples
 
-        # set grad to approximated grad
-        objective.grads = g_0
+#         # set grad to approximated grad
+#         objective.grads = g_0
 
-        # update H
-        H = self.global_state.get("H", None)
-        if H is None: H = H_hat
-        else:
-            beta = self.defaults["beta"]
-            if beta is None: beta = k / (k+1)
-            H.lerp_(H_hat, 1-beta)
+#         # update H
+#         H = self.global_state.get("H", None)
+#         if H is None: H = H_hat
+#         else:
+#             beta = self.defaults["beta"]
+#             if beta is None: beta = k / (k+1)
+#             H.lerp_(H_hat, 1-beta)
 
-        self.global_state["H"] = H
+#         self.global_state["H"] = H
 
-    @torch.no_grad
-    def apply(self, objective):
-        dir = _newton_step(
-            objective=objective,
-            H = self.global_state["H"],
-            damping = self.defaults["damping"],
-            inner = self.children.get("inner", None),
-            H_tfm=None,
-            eigval_fn=self.defaults["eigval_fn"],
-            use_lstsq=self.defaults["use_lstsq"],
-            g_proj=None,
-        )
+#     @torch.no_grad
+#     def apply(self, objective):
+#         dir = _newton_step(
+#             objective=objective,
+#             H = self.global_state["H"],
+#             damping = self.defaults["damping"],
+#             inner = self.children.get("inner", None),
+#             H_tfm=None,
+#             eigval_fn=self.defaults["eigval_fn"],
+#             use_lstsq=self.defaults["use_lstsq"],
+#             g_proj=None,
+#         )
 
-        objective.updates = vec_to_tensors(dir, objective.params)
-        return objective
+#         objective.updates = vec_to_tensors(dir, objective.params)
+#         return objective
 
-    def get_H(self,objective=...):
-        return _get_H(self.global_state["H"], self.defaults["eigval_fn"])
+#     def get_H(self,objective=...):
+#         return _get_H(self.global_state["H"], self.defaults["eigval_fn"])
