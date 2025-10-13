a0119ec9e494468370db334b3dee61277fe48d95
Ivan Nikishev <nkshv2@gmail.com>
2025-10-13 17:32:05 +0300

    removed some experimental optimizers as they will be added to new Basis API

--- torchzero/modules/grad_approximation/__init__.py
+++ torchzero/modules/grad_approximation/__init__.py
@@ -1,4 +1,5 @@
-from .grad_approximator import GradApproximator, GradTarget
 from .fdm import FDM
-from .rfdm import RandomizedFDM, MeZO, SPSA, RDSA, GaussianSmoothing
 from .forward_gradient import ForwardGradient
+from .grad_approximator import GradApproximator, GradTarget
+from .rfdm import RDSA, SPSA, GaussianSmoothing, MeZO, RandomizedFDM
+from .spsa1 import SPSA1
